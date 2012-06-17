c5d5c785ddd298655a4efcafa52084ec4ae095f0
jmhdez <juan.hernandez.arroyo@gmail.com>
2012-06-17 20:42:33 +0200

    added initial code

---
+++ src/TinyTwitter/TinyTwitterTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TinyTwitter
+{
+	public class TinyTwitterTests
+	{
+		// TODO: Add integration tests against twitter
+	}
+}
